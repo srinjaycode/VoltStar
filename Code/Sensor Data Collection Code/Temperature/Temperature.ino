8201bbb1afb7376e62141ef1d2f6de140c557699
Ahmed28309 <ahmed.rizsoft@gmail.com>
2025-01-28 12:54:56 +0400

    Temperature Code Fixed

--- Code/Sensor Data Collection Code/Temperature/Temperature.ino
+++ Code/Sensor Data Collection Code/Temperature/Temperature.ino
@@ -1,29 +1,81 @@
-
-//Include libraries
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 2
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
+// Data wire is connected to Arduino Nano pins
+#define ONE_WIRE_BUS_CONTROLLER    4
+#define ONE_WIRE_BUS_BATTERY       3
+
+// Setup a oneWire instance for each bus
+OneWire oneWireController(ONE_WIRE_BUS_CONTROLLER);
+OneWire oneWireBattery(ONE_WIRE_BUS_BATTERY);
+
+// Pass our oneWire reference to Dallas Temperature sensor objects
+DallasTemperature controllerSensor(&oneWireController);
+DallasTemperature batterySensor(&oneWireBattery);
+
+// Arrays to hold device addresses
+DeviceAddress controllerThermometer, batteryThermometer;
 
 void setup(void)
 {
-  Serial.begin(9600); //Begin serial communication
-  Serial.println("Arduino Digital Temperature // Serial Monitor Version"); //Print a message
-  Serial.println("ignore"); // ignore yay
-  sensors.begin();
+  // Start serial port
+  Serial.begin(115200);
+  Serial.println("Controller and Battery Temperature Sensors");
+  
+  // Start up the library on both buses
+  controllerSensor.begin();
+  batterySensor.begin();
+  
+  // Locate devices on the bus
+  Serial.println("Locating devices...");
+  Serial.print("Found ");
+  Serial.print(controllerSensor.getDeviceCount(), DEC);
+  Serial.println(" devices on controller bus.");
+  Serial.print("Found ");
+  Serial.print(batterySensor.getDeviceCount(), DEC);
+  Serial.println(" devices on battery bus.");
+
+  // Search for devices on the bus and assign addresses
+  if (!controllerSensor.getAddress(controllerThermometer, 0)) {
+    Serial.println("Unable to find address for controller sensor");
+  }
+  if (!batterySensor.getAddress(batteryThermometer, 0)) {
+    Serial.println("Unable to find address for battery sensor");
+  }
+
+  // Set the resolution to 12 bit (good enough for most applications)
+  controllerSensor.setResolution(controllerThermometer, 12);
+  batterySensor.setResolution(batteryThermometer, 12);
 }
 
 void loop(void)
 { 
-  // Send the command to get temperatures
-  sensors.requestTemperatures();  
-  Serial.print("Temperature is: ");
-  Serial.println(sensors.getTempCByIndex(0)); // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
-  //Update value every 1 sec.
-  delay(1000);
+  // Request temperatures from both sensors
+  Serial.println("Requesting temperatures...");
+  controllerSensor.requestTemperatures();
+  batterySensor.requestTemperatures();
+  
+  // Get and print temperatures
+  float tempC_controller = controllerSensor.getTempC(controllerThermometer);
+  float tempC_battery = batterySensor.getTempC(batteryThermometer);
+  
+  // Check if reading was successful
+  if(tempC_controller != DEVICE_DISCONNECTED_C) {
+    Serial.print("Controller Temperature: ");
+    Serial.print(tempC_controller);
+    Serial.println("°C");
+  } else {
+    Serial.println("Error reading controller temperature");
+  }
+  
+  if(tempC_battery != DEVICE_DISCONNECTED_C) {
+    Serial.print("Battery Temperature: ");
+    Serial.print(tempC_battery);
+    Serial.println("°C");
+  } else {
+    Serial.println("Error reading battery temperature");
+  }
+  
+  // Wait a bit between readings
+  delay(2000);
 }
