4740ad4a9e81051cb62a63b704fb93687a9ae653
Srinjay <srinjaycode@gmail.com>
2025-02-18 15:38:53 +0400

    Updated IMU.h, works for arduino nano 33 IoT

--- Voltstar Telemetry System/IMU.h
+++ Voltstar Telemetry System/IMU.h
@@ -1,19 +1,13 @@
 #ifndef IMU_H
 #define IMU_H
 
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-#include <Wire.h>
+#include <Arduino_LSM6DS3.h>
 
-// IMU Sensor
-extern Adafruit_MPU6050 mpu;
-
-// IMU Calibration and Filtering Variables
-extern float axOffset, ayOffset, azOffset;
+// IMU Sensor Variables
 extern float axFiltered, ayFiltered, azFiltered;
-extern bool mpuConnected;
+extern bool imuConnected;
 
-void calibrateSensor();
+void initializeIMU();
 void getIMU();
 
 #endif
