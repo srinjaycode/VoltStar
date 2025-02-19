22ee1d9711f9d10ace1c5f8356b7791f35bf7557
yahiaezzat121 <yahiaezzat2009@gmail.com>
2025-02-19 19:56:19 +0400

    new

--- LCD Display/lcd_code.ino
+++ LCD Display/lcd_code.ino
@@ -1,7 +1,4 @@
-/* Updated Code with
- refreshing temperature and speed data
-  from the nano */
-  
+// new
 #include <MCUFRIEND_kbv.h>
 #include <Adafruit_GFX.h>
 
@@ -10,73 +7,107 @@
 
 MCUFRIEND_kbv tft;
 
+// Global variables for display values
 bool showMetrics = false;
-int velocity = 0, batteryTemp = 0, controllerTemp = 0;
+int velocity = 0;
+int batteryTemp = 0;
+int controllerTemp = 0;
 
 void setup() {
-  Serial.begin(9600);
-  uint16_t ID = tft.readID();
-  tft.begin(ID);
-  tft.setRotation(3);
-  tft.fillScreen(0x0000);
-  
-  drawHomeScreen();
-  delay(3000);
-  showMetrics = true;
-  drawMetricsScreen();
+    // Initialize serial communication with Mega
+    Serial.begin(9600);
+    
+    // Initialize display
+    uint16_t ID = tft.readID();
+    tft.begin(ID);
+    tft.setRotation(3);
+    tft.fillScreen(0x0000);  // Black background
+    
+    // Show startup screen
+    drawHomeScreen();
+    delay(3000);
+    
+    // Switch to metrics display
+    showMetrics = true;
+    drawMetricsScreen();
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\n');
-    parseData(data);
-    updateDisplay();
-  }
+    // Check for incoming data from Mega
+    if (Serial.available() > 0) {
+        String data = Serial.readStringUntil('\n');
+        parseData(data);
+        updateDisplay();
+    }
 }
 
 void parseData(String data) {
-  if (data.startsWith("V")) {
-    velocity = data.substring(1).toInt();
-  } else if (data.startsWith("BT")) {
-    batteryTemp = data.substring(2).toInt();
-  } else if (data.startsWith("CT")) {
-    controllerTemp = data.substring(2).toInt();
-  }
+    // Find the position of the colon separator
+    int colonIndex = data.indexOf(':');
+    
+    if (colonIndex != -1) {
+        // Split the data into header and value
+        String header = data.substring(0, colonIndex);
+        String value = data.substring(colonIndex + 1);
+        
+        // Update the appropriate variable
+        if (header == "V") {
+            velocity = value.toInt();
+        } else if (header == "BT") {
+            batteryTemp = value.toInt();
+        } else if (header == "CT") {
+            controllerTemp = value.toInt();
+        }
+        
+        // Debug output
+        Serial.println("Received: " + data);
+    }
 }
 
 void drawHomeScreen() {
-  tft.fillScreen(0x0000);
-  String text1 = "Volt", text2 = "Star";
-  tft.setTextSize(7);
-  tft.setTextColor(0xFFFF);
-  drawBoldText(60, 120, text1, 2);
-  tft.setTextColor(0xFFDF00);
-  drawBoldText(160, 120, text2, 2);
+    tft.fillScreen(0x0000);  // Black background
+    
+    // Draw VoltStar logo
+    String text1 = "Volt";
+    String text2 = "Star";
+    
+    tft.setTextSize(7);
+    
+    // Draw "Volt" in white
+    tft.setTextColor(0xFFFF);
+    drawBoldText(60, 120, text1, 2);
+    
+    // Draw "Star" in yellow
+    tft.setTextColor(0xFFDF00);
+    drawBoldText(160, 120, text2, 2);
 }
 
 void drawMetricsScreen() {
-  tft.fillScreen(0x0000);
-  updateDisplay();
+    tft.fillScreen(0x0000);
+    updateDisplay();
 }
 
 void drawBoldText(int x, int y, String text, int offset) {
-  for (int i = -offset; i <= offset; i++) {
-    for (int j = -offset; j <= offset; j++) {
-      tft.setCursor(x + i, y + j);
-      tft.print(text);
+    // Draw text multiple times with slight offsets for bold effect
+    for (int i = -offset; i <= offset; i++) {
+        for (int j = -offset; j <= offset; j++) {
+            tft.setCursor(x + i, y + j);
+            tft.print(text);
+        }
     }
-  }
 }
 
 void updateDisplay() {
-  tft.fillScreen(0x0000);
-  
-  tft.setTextSize(7);
-  tft.setTextColor(0xFFDF00);
-  drawBoldText(120, 100, String(velocity) + " km/h", 3);
-
-  tft.setTextSize(5);
-  tft.setTextColor(0xFFFF);
-  drawBoldText(120, 220, "B:" + String(batteryTemp) + "C", 1);
-  drawBoldText(120, 270, "C:" + String(controllerTemp) + "C", 1);
+    tft.fillScreen(0x0000);  // Clear screen
+    
+    // Draw speed in large yellow text
+    tft.setTextSize(7);
+    tft.setTextColor(0xFFDF00);
+    drawBoldText(120, 100, String(velocity) + " km/h", 3);
+    
+    // Draw temperatures in white text
+    tft.setTextSize(5);
+    tft.setTextColor(0xFFFF);
+    drawBoldText(120, 220, "B:" + String(batteryTemp) + "C", 1);
+    drawBoldText(120, 270, "C:" + String(controllerTemp) + "C", 1);
 }
